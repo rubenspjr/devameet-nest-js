5004b909d317c937a5fcb0b4c1c8b5ffdccbffd6
Rubens Pinheiro <torettodev@gmail.com>
2023-06-01 10:18:20 -0300

    aula03 parte 2

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,6 +3,7 @@ import { HttpCode, HttpStatus, Body} from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { LoginDto } from "./dtos/login.dto";
 import { ResgisterDto } from "src/user/dtos/register";
+import { IsPublic } from "./decorators/ispublic.decorator";
 
 
 @Controller('auth')
@@ -11,12 +12,14 @@ export class AuthController{
   
   @Post('login')
   @HttpCode(HttpStatus.OK)
+  @IsPublic()
   login(@Body() dto: LoginDto){
     return this.authService.login(dto);
   }
 
   @Post('register')
   @HttpCode(HttpStatus.OK)
+  @IsPublic()
   register(@Body() dto: ResgisterDto){
     return this.authService.register(dto);
   }
