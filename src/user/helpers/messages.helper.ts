6b2a7f121cdff81d6b25f9783559b3af1e1f977f
Rubens Pinheiro <torettodev@gmail.com>
2023-06-01 10:38:56 -0300

    Aula 3 parte 3

--- src/user/helpers/messages.helper.ts
+++ src/user/helpers/messages.helper.ts
@@ -3,5 +3,6 @@ export const UserMessagesHelper = {
     REGISTER_NAME_NOT_VALID : 'Nome informado não é válido',
     REGISTER_EMAIL_NOT_VALID : 'Email informado não é válido',
     REGISTER_PASSWORD_NOT_VALID : 'Senha informada não é válida, ela precisa ter de 4 a 12 caracteres e conter pelo menos um caractér maiúsculo, um minúsculo e um carctér especial',
-    REGISTER_EXIST_EMAIL_ACCOUNT : 'Já existe uma conta com o email informado'
+    REGISTER_EXIST_EMAIL_ACCOUNT : 'Já existe uma conta com o email informado',
+    GET_USER_NOT_FOUND: 'Usuario não encontrado',
 }
